26849352bd4b2b6164cb4c6404ec113dd51f62d1
halbeisent <dev@halbeisen.fr>
2018-05-16 12:03:46 +0200

    Exercice 2 Partie 1 PHP terminé et corrigé

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8" />
+    <link rel="stylesheet" href="style.css">
+    <title>Exercice 2 Partie 1 PHP</title>
+  </head>
+  <body>
+  <?php
+  $age = 24;
+  $firstName = 'John';
+  $lastName = 'Doe';
+  echo 'la personne se nomme ' . $firstName . ' ' . $lastName . ' et a ' . $age . ' ans';
+  ?>
+  </body>
+</html>
